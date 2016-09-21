b30a923cbf634520fd37ba6e941785dcf7059a00
keen <ehrudxo@hotmail.com>
2016-09-21 16:45:40 +0900

    for doodle now

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import firebase from 'firebase';
+import config from './config';
+
+firebase.initializeApp(config);
+
+class App extends Component {
+  componentWillMount() {
+    this.firebaseRef = firebase.database().ref("items");
+    this.firebaseRef.on("child_added", function(dataSnapshot) {
+      this.items.push(dataSnapshot.val());
+      this.setState({
+        items: this.items
+      });
+    }.bind(this));
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.firebaseRef.push({
+      text: this.state.text
+    });
+    this.setState({text: ""});
+  }
+  componentWillUnmount() {
+    this.firebaseRef.off();
+  }
+  onChange(e) {
+    var stateObj = {};
+    stateObj[e.target.name]=e.target.value;
+    this.setState(stateObj);
+   }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Doodle Now</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <DoodleList items={ this.state.items } removeItem={ this.removeItem } />
+        <form onSubmit={ this.handleSubmit }>
+          title : <input name="title" onChange={ this.onChange } value={ this.state.title } /><br/>
+          content : <input name="content" onChange={ this.onChange } value={ this.state.content } /><br/>
+          url : <input name="url" onChange={ this.onChange } value={ this.state.url } /><br/>
+          <button>{ 'Add #' + (this.state.items.length + 1) }</button>
+        </form>
+      </div>
+    );
+  }
+}
+var DoodleList = React.createClass({
+  render: function() {
+    var _this = this;
+    var createItem = function(item, index) {
+      return (
+        <li key={ index }>
+          { item.text }
+          <span onClick={ _this.props.removeItem.bind(null, item['.key']) }
+                style={{ color: 'red', marginLeft: '10px', cursor: 'pointer' }}>
+            X
+          </span>
+        </li>
+      );
+    };
+    return <ul>{ this.props.items.map(createItem) }</ul>;
+  }
+});
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,8 @@
+ var config = {
+  apiKey: "AIzaSyB4C6e9NEKEIXgjoE-4J_nk0Mzs5XpgoCM",
+  authDomain: "dnow-ba53c.firebaseapp.com",
+  databaseURL: "https://dnow-ba53c.firebaseio.com",
+  storageBucket: "dnow-ba53c.appspot.com",
+  messagingSenderId: "1015110234102"
+}
+export default config;
