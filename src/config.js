663bab8b5eb66076ad4c22a89c560a4bb57e9673
keen <ehrudxo@hotmail.com>
2016-10-16 16:20:38 +0900

    Updates

--- src/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
- apiKey: "AIzaSyDTWKZgWTSQQ68IspjaQTv15pOXr6JOFd8",
- databaseURL: "https://doodle-now.firebaseio.com",
- storageBucket: "doodle-now.appspot.com",
- messagingSenderId: "220022746007",
- embedlyKey :"c5baaf5335ed41ddb95686e59be3331e"
-}
-export default config;
